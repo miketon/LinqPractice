2de3998fb331d2d0f623e4f5beb7651c07e60bdc
Mike Ton <mike.ton@gmail.com>
2016-04-26 22:10:49 -0700

    Linq Practice.

---
+++ LinqPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinqPractice {
+  class MainClass {
+    public static void Main () {
+      DateTime date = DateTime.Parse("3/24/2025");
+      DateTime time = DateTime.Parse("1/1/0001 9:55pm");
+      Console.WriteLine ("Hello Date! {0}", date);
+      Console.WriteLine ("Hello Time! {0}", time);
+      DateTime combined = new DateTime(date.Year, date.Month, date.Day, time.Hour, time.Minute, time.Second);
+      Console.WriteLine ("Combined date and time! {0}", combined);
+    }
+  }
+}
