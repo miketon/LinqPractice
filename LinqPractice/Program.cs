b16c59a248f16e4351f896f5a288d3b90cdae89c
Mike Ton <mike.ton@gmail.com>
2016-04-27 21:22:14 -0700

    gitignore not working?

--- LinqPractice/Program.cs
+++ LinqPractice/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 
 namespace LinqPractice {
-  static class MainClass {
+
+  static class MyExtensionHelpers {
     
-    static DateTime Combined(this DateTime datePart, DateTime timePart){
+    public static DateTime Combined(this DateTime datePart, DateTime timePart){
       return new DateTime(
         datePart.Year, datePart.Month, datePart.Day,
         timePart.Hour, timePart.Minute, timePart.Millisecond
       );
     }
+  }
+
+  static class MainClass {
 
     public static void Main () {
       DateTime date = DateTime.Parse("3/24/2025");
@@ -16,7 +20,7 @@ namespace LinqPractice {
       Console.WriteLine ("Hello Date! {0}", date);
       Console.WriteLine ("Hello Time! {0}", time);
       DateTime combined = new DateTime(date.Year, date.Month, date.Day, time.Hour, time.Minute, time.Second);
-      DateTime combinedFunc = Combined(date, time);
+      DateTime combinedFunc = MyExtensionHelpers.Combined(date, time);
       DateTime combinedFuncExtended = date.Combined(time); //sugar
       Console.WriteLine ("Combined date and time! {0}", combined);
       Console.WriteLine ("Combined date and time Function! {0}", combinedFunc);
