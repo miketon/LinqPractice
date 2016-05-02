1cd8002a8f6c8a7fecf6a28e88611f9f8da9ca45
Mike Ton <mike.ton@gmail.com>
2016-05-01 22:11:46 -0700

    Writing my own Where using Extension methods.

--- LinqPractice/HelloLinq.cs
+++ LinqPractice/HelloLinq.cs
@@ -1,7 +1,22 @@
 ﻿using System;
 using System.Linq; //extension method for array that allows Where and Select
+using System.Collections.Generic; //needed for IEnumerable
 
 namespace LinqPractice {
+
+  static public class MTON{ 
+
+    // Writing our own Where extension.
+    // Function will search this context first before System.Linq
+    static public IEnumerable<int> Where(this int[] ints, Func<int, bool> predicate){
+      Console.WriteLine("MyWhere : {0}", ints);
+      foreach(int i in ints){
+        if(predicate(i)){
+          yield return i;
+        }
+      }
+    }
+  }
 
   public class HelloLinq {
 
@@ -14,7 +29,7 @@ namespace LinqPractice {
         where n < 5      
         select n;
 
-      // Less sugar - Using Linq extension to Arrays
+      // Less sugar - Using Linq extension to Arrays (which implements IEnumerable)
       var resultLessSugar = 
         numbers
           .Where(n => n < 5)
@@ -23,9 +38,14 @@ namespace LinqPractice {
       // No sugar - Using Enumerable
       var noSugar =
         Enumerable.Select(
-          Enumerable.Where(numbers, n => n < 5), 
+//          Enumerable.Where(numbers, n => n < 5), 
+          MTON.Where(numbers, n => n < 5), 
           n => n
         );
+
+      foreach(int i in noSugar){
+        Console.WriteLine("No Sugar : {0} with MTON.Where", i);
+      }
 
       foreach(int i in result){
         Console.WriteLine("HelloLinq : {0}", i);
