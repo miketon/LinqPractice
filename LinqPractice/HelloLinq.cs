0a68e1ef4522bbff6b03a881dedb9f4b2317d371
Mike Ton <mike.ton@gmail.com>
2016-05-04 22:25:06 -0700

    Moving the Where Random to actually run.

--- LinqPractice/HelloLinq.cs
+++ LinqPractice/HelloLinq.cs
@@ -89,7 +89,7 @@ namespace LinqPractice {
         from n in numbers
         where n < 5 
         where n > 0
-        where new Random().Next() == 23
+//        where new Random().Next() == 23
         orderby n
         orderby n * 2 + 3
         orderby 5
@@ -102,7 +102,7 @@ namespace LinqPractice {
           .OrderBy(n => 8)
           .Where(n => n < 5)
           .Where(n => n > 0)
-          .Where(n => new Random().Next()==23)
+//          .Where(n => new Random().Next()==23)
           .OrderBy(n => n)
           .OrderBy(n => n*2+3)
           .OrderBy(n => 5);
@@ -114,11 +114,12 @@ namespace LinqPractice {
         Enumerable.OrderBy(
           Enumerable.OrderBy(
             Enumerable.OrderBy(
-              Enumerable.Where(
+//              Enumerable.Where(
                 Enumerable.Where(
                   Enumerable.Where(
                     Enumerable.OrderBy(numbers, n => 8), n => n < 5), n => n > 0), 
-                    n => new Random().Next()==23), n => n), n => n*2+3), n => 5);
+//                    n => new Random().Next()==23), 
+                    n => n), n => n*2+3), n => 5);
 //          .Select(n => n); // degenerate, compiler removes
 
       foreach(int i in result1){
