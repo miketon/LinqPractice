3e6ff2d2eeceab40a22a6ce7683fee4a2c61b7ad
Mike Ton <mike.ton@gmail.com>
2016-05-07 21:03:54 -0700

    Adding AssemblyLine.cs

---
+++ LinqPractice/AssemblyLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqPractice {
+  public class AssemblyLine {
+    public AssemblyLine () {
+      Console.WriteLine("Assembly Line");
+      int[] stuff = {4, 13, 8, 1, 9};
+      var result = stuff
+        .Where(i => i < 10)
+        .Where(i => 4 < 1)
+        .Select(i => 1 * 3)
+        .Where(i => i % 2 == 0)
+        .Select(i => i + "Mike");
+
+      IEnumerator<string> rator = result.GetEnumerator();
+
+      while(rator.MoveNext()){
+        Console.WriteLine("rator {0}", rator.Current);
+      }
+    }
+  }
+}
+
